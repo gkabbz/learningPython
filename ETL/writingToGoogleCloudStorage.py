3baa400857aedc22fcf60135b78f88e251a29103
gkabbz <george@akisha.ca>
2019-03-02 14:17:07 -0500

    added ETL folder and miscelaneous changes

---
+++ ETL/writingToGoogleCloudStorage.py
@@ -0,0 +1,34 @@
+from google.cloud import storage
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= '/Users/gkaberere/Google Drive/Github/marketing-analytics/App Engine - moz-mktg-prod-001/moz-mktg-prod-001-app-engine-GAMozillaProdAccess.json'
+
+storage_client = storage.Client()
+bucket = storage_client.get_bucket("snippets-data-transfer") #bucket name
+blob = bucket.blob('metaData/snippets_metadata_20190227h.csv') # The folder / filename of where you want the file saved in cloud storage
+blob.upload_from_filename(filename='/users/gkaberere/spark-warehouse/testSnippet/metaData/snippets_metadata_20190227.csv')
+
+# Get list of buckets
+buckets = list(storage_client.list_buckets())
+print(buckets)
+
+
+
+# TODO: Function to upload to google cloud storage
+bucket = 'snippets-data-transfer'
+blobname = f'metaData/20190227.csv'
+csv_file_location = f'/users/gkaberere/spark-warehouse/testSnippet/metaData/snippets_metadata_20190227.csv'
+
+def upload_to_gcs(csvfile, bucket, blobname):
+   client = storage.Client()
+   bucket = client.get_bucket(bucket)
+   blob = bucket.blob(blobname)
+   blob.upload_from_filename(filename=csvfile)
+   gcslocation = f'gs://{bucket}/{blobname}'
+   logging.info(f'Uploaded {gcslocation} ...')
+   return gcslocation
+
+#csvfile = download_metadata_file(url, temp_file_name)
+upload_to_gcs(csv_file_location, bucket, blobname)
+
+
